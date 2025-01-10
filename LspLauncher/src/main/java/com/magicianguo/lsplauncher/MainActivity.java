363dbe2a9bab5e690cf1f384631b908f90b8cce8
guojc <guojc@meixing.com>
2025-01-10 10:02:31 +0800

    执行后定时关闭应用

--- LspLauncher/src/main/java/com/magicianguo/lsplauncher/MainActivity.java
+++ LspLauncher/src/main/java/com/magicianguo/lsplauncher/MainActivity.java
@@ -3,16 +3,29 @@ package com.magicianguo.lsplauncher;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.widget.Toast;
 
 import java.io.OutputStream;
 
 public class MainActivity extends Activity {
+    private static final int MSG_KILL_PROCESS = 1;
+    private static final Handler sHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_KILL_PROCESS) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         boolean hasRoot = false;
+        boolean execSuccess = false;
         try {
             Process process = Runtime.getRuntime().exec("su");
             hasRoot = true;
@@ -26,6 +39,7 @@ public class MainActivity extends Activity {
             os.write(cmd.getBytes());
             os.flush();
             os.close();
+            execSuccess = true;
         } catch (Exception e) {
             e.printStackTrace();
             String toastMessage;
@@ -35,8 +49,19 @@ public class MainActivity extends Activity {
                 toastMessage = "请授予Root权限！";
             }
             Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
-        } finally {
-            finish();
         }
+        finish();
+
+        // 执行后，定时关闭应用
+        if (execSuccess) {
+            killProcess(10 * 1000);
+        } else {
+            killProcess(3 * 1000);
+        }
+    }
+
+    private void killProcess(long delayMillis) {
+        sHandler.removeMessages(MSG_KILL_PROCESS);
+        sHandler.sendEmptyMessageDelayed(MSG_KILL_PROCESS, delayMillis);
     }
 }
